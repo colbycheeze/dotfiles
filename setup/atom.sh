76725c8d5c888a136693fe8b7bd3f1ce0c10f567
Colby Williams <colby@colbycheeze.com>
2017-01-29 09:08:55 -0600

    Upgraded to vim-mode-plus

--- setup/atom.sh
+++ setup/atom.sh
@@ -6,7 +6,6 @@ ln -s ~/dotfiles/atom ~/.atom
 brew cask install atom
 
 fancy_echo "Installing Atom packages"
-apm install atom-keyboard-macros-vim
 apm install autocomplete-modules
 apm install autocomplete-paths
 apm install color-picker
@@ -19,5 +18,7 @@ apm install linter-eslint
 apm install merge-conflicts
 apm install pigments
 apm install relative-numbers
-apm install vim-mode
-apm install vim-surround
+apm install atom-keyboard-macros-vim
+apm install vim-mode-plus
+apm install vim-mode-plus-keymaps-for-surround
+apm install gist
